6188014390d6873635a6d09ad3b128341397c13e
bluet52hzzz <lionel32204@gmail.com>
2025-02-24 23:28:42 +0700

    meo meo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -166,7 +166,7 @@ const Index = () => {
             <div className="flex items-center justify-center space-x-2 text-event-gray">
               <Clock className="w-5 h-5" />
               <span>
-                {EventDetails.date} • {EventDetails.time}
+                {EventDetails.date} • {EventDetails.time} 
               </span>
             </div>
           </div>
